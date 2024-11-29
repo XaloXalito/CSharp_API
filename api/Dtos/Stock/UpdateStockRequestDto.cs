8cde3e62044f87a444014f4465da1d437b8ea658
Adrian Rodriguez <adrianggrodriguez@gmail.com>
2024-11-29 13:55:03 -0400

    Update 29/11/2024

--- api/Dtos/Stock/UpdateStockRequestDto.cs
+++ api/Dtos/Stock/UpdateStockRequestDto.cs
@@ -2,16 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using api.Dtos.Comment;
 
 namespace api.Dtos.Stock
 {
-    public class StockDto
+    public class UpdateStockRequestDto
     {
-        public int Id { get; set; }
-        
         public string Symbol { get; set; } = string.Empty;
-
         public string CompanyName { get; set; } = string.Empty;
 
         public decimal Purchase { get; set; }
@@ -19,9 +15,7 @@ namespace api.Dtos.Stock
         public decimal LastDiv { get; set; }
 
         public string Industry { get; set; } = string.Empty;
-        
+
         public long MarketCap { get; set; }
-        
-        public List<CommentDto> Comments {get; set;}
     }
 }
--- api/Program.cs
+++ api/Program.cs
@@ -1,5 +1,8 @@
 using api.Data;
+using api.interfaces;
+using api.Repository;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,11 +11,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+builder.Services.AddControllers().AddNewtonsoftJson(options => {
+    options.SerializerSettings.ReferenceLoopHandling=ReferenceLoopHandling.Ignore;
+});
 
 builder.Services.AddDbContext<ApplicationDBContext>(options => 
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 
 var app = builder.Build();
 
